3cd191c652cc34840231cda7a26573ac61f21e8c
Femicode24 <62211699+Femipython@users.noreply.github.com>
2020-03-25 05:46:23 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Justfemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Full Name: Ajibade Olufemi Olawale");
+
+            Console.WriteLine("Email: ajibadeolufemi@gmail.com")
+
+            Console.WriteLine("Slack username: Justfemi")
+
+            Console.WriteLine("Track: Backend track")
+
+            Console.WriteLine("Courses: Python and C#")
+        }
+    }
+}
